d781016ffbee7ab865da43f4727e42080dd9da1c
Jaime Barreto <yaimesil@gmail.com>
2026-08-13 13:03:09 +0100

    chore: enable react-native-worklets bundle mode for hermes v1

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,16 @@
+/** @type {import('react-native-worklets/plugin').PluginOptions} */
+const workletsPluginOptions = {
+  bundleMode: true,
+  strictGlobal: true,
+};
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-    plugins: [['inline-import', { extensions: ['.sql'] }]],
+    plugins: [
+      ['inline-import', { extensions: ['.sql'] }],
+      ['react-native-worklets/plugin', workletsPluginOptions],
+    ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { getBundleModeMetroConfig } = require('react-native-worklets/bundleMode');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts.push('sql');
 
-module.exports = withNativeWind(config, { input: './styles/global.css' });
+module.exports = withNativeWind(getBundleModeMetroConfig(config), {
+  input: './styles/global.css',
+});
