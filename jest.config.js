bb4ed28fb74c85b63208a5f244add92785279927
Jaime Barreto <yaimesil@gmail.com>
2026-08-24 13:09:27 +0100

    chore: keep jest out of git worktree checkouts

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   clearMocks: true,
+  modulePathIgnorePatterns: ['<rootDir>/.worktrees/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ['<rootDir>/.worktrees/'],
 };
